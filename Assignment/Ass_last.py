96c7964ae6b62132f690dee37b1b89640bef9727
zerosheepmoo <zerosheepmoo@gmail.com>
2019-12-20 00:08:59 +0900

    ass_last instruction 수정

--- Assignment/Ass_last.py
+++ Assignment/Ass_last.py
@@ -17,7 +17,7 @@
 # 편집
 
 # searchID(id: string): 무기(id를 포함하는 상품코드를 갖고 있는 무기의 리스트 출력) 검색
-# searchG(more: number | false, less: number | false, equal: int | false: 가격(이상, 이하, 같을 때), false 일땐, 해당 필터링 X
+# searchG(more: number || false, less: number || false, equal: int || false: 가격(이상, 이하, 같을 때), false 일땐, 해당 필터링 X
 # editG(id: string, G: number): id와 상품코드랑 일치하는 무기 가격 수정 
 # weaponAdd(type: string, name: string, id: number, G: number, etc?: string ): 지금 편집하고 있는 리스트에 대해 추가 (etc는 optional)
 # weaponDelete(id: number): 해당하는 id를 가진 무기를 리스트에서 삭제
